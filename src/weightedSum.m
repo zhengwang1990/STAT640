86849dbcf3ff3012dce31152d8bf32eac39d7a1a
zw14 <zheng.wang.rice@gmail.com>
2015-09-11 22:48:27 -0500

    fast KNN & weighted sum based on variance

---
+++ src/weightedSum.m
@@ -0,0 +1,26 @@
+function pFinal = weightedSum(pMean, pKNN)
+global rmat;
+global predInd;
+global Npred;
+
+Nprofile = size(rmat,2);
+
+Pnum = sum(rmat~=0,1);
+Psum = sum(rmat,1);
+Pmeans = full(Psum./Pnum);
+[IA, JA, AA]=sparse_to_csr(rmat');
+var = zeros(Nprofile,1);
+for ip = 1:Nprofile    
+    x = AA(IA(ip):IA(ip+1)-1);
+    x = x - Pmeans(ip);
+    var(ip) = (x'*x)/Pnum(ip);
+end
+
+pFinal = zeros(Npred,1);
+for ip = 1:Npred
+    variance = var(predInd(ip,2));
+    alpha = 0.5*variance/10;
+    pFinal(ip) = min(max((0.5-alpha)*pMean(ip) + (0.5+alpha)*pKNN(ip),1),10);
+end
+
+end
